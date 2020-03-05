86a8aca13c430add5482aab746d51549c7364706
AS-AlStar <starovojtov.alexander@gmail.com>
2020-03-05 14:15:38 +0300

    Fix logic for user_ips_controller

--- app/controllers/api/v1/user_ips_controller.rb
+++ app/controllers/api/v1/user_ips_controller.rb
@@ -7,6 +7,7 @@ module Api
         list = Post.joins(:user)
                    .group(:user_ip)
                    .where('users.posts_count > 1')
+                   .having('count(users.login) > 1')
                    .select('user_ip, array_agg(users.login) as logins')
         list = list.paginate(page: params[:page], per_page: 30) unless shown_all?
         render json: UserIpsListSerializer.new(list).serializable_hash, status: :ok
